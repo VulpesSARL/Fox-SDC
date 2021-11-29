a6d11e9666e2e35745ee42107d6535d99939d85d
vulpesfox <support@vulpes.lu>
2021-11-29 16:47:54 +0100

    Updated universal package script

--- FoxSDC_PackageCreator/PackageScriptTemplate.cs
+++ FoxSDC_PackageCreator/PackageScriptTemplate.cs
@@ -1,6 +1,8 @@
 ﻿using FoxSDC_Common;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace FoxSDC_Package
@@ -8,23 +10,61 @@ namespace FoxSDC_Package
     public class PackageScriptTemplate : PKGScript
     {
         PKGInstallState State = PKGInstallState.NotSet;
+        const string RegPath = "Software\\Fox\\SDC Scripted Installs\\";
+
+        PKGStatus CheckReq(PKGRunningPackageData Package)
+        {
+            using (RegistryKey r = Registry.LocalMachine.OpenSubKey(RegPath))
+            {
+                if (r == null)
+                    return (PKGStatus.Success);
+                string Current = Convert.ToString(r.GetValue(Package.PackageID.Replace("\\", ""), ""));
+                Int64 CurrentV;
+                if (Int64.TryParse(Current, out CurrentV) == false)
+                    //install it!
+                    return (PKGStatus.Success);
+
+                if (CurrentV <= Package.VersionID)
+                    //nö, not needed
+                    return (PKGStatus.NotNeeded);
+            }
+
+            //install it!
+            return (PKGStatus.Success);
+        }
 
         public PKGStatus CheckInstallationStatus(PKGRunningPackageData Package, PKGInstallState state)
         {
             State = state;
             Package.SetInstallPath("%PROGRAMFILES%\\Fox\\My Package");
             Package.ErrorText = "";
-            return (PKGStatus.Success);
+
+            PKGStatus p = CheckReq(Package);
+            if (p != PKGStatus.Success)
+                Package.ErrorText = "Not needed";
+
+            return (p);
         }
 
         public PKGStatus PreInstall(PKGRunningPackageData Package)
         {
-            Package.ErrorText = "";
-            return (PKGStatus.Success);
+            PKGStatus p = CheckReq(Package);
+            if (p != PKGStatus.Success)
+                Package.ErrorText = "Not needed";
+
+            return (p);
         }
 
         public PKGStatus PostInstall(PKGRunningPackageData Package)
         {
+            using (RegistryKey r = Registry.LocalMachine.CreateSubKey(RegPath))
+            {
+                if (r != null)
+                {
+                    r.SetValue(Package.PackageID.Replace("\\", ""), Package.VersionID);
+                }
+            }
+
             Package.ErrorText = "";
             return (PKGStatus.Success);
         }
@@ -32,7 +72,7 @@ namespace FoxSDC_Package
         public PKGStatus Rollback(PKGRunningPackageData Package)
         {
             Package.ErrorText = "";
-            return (PKGStatus.Success);            
+            return (PKGStatus.Success);
         }
 
         public List<string> GetDependencies(PKGRunningPackageData Package)
